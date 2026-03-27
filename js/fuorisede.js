19a11f973b858ff781a011ea2612bdaf8137ecc3
Educatori <educatori127@gmail.com>
2026-03-27 12:18:20 +0000

    Deploying to gh-pages from @ Educatori/convitto2025@2ee3081d80b1dd386e76f570d99f1a451e7e2a0f 🚀

---
+++ js/fuorisede.js
@@ -0,0 +1,5 @@
+const fuorisede = [
+    { cognome: "A-HA", nome: "Marco", classe: "1A", gruppo: "", room: "semi" },
+    { cognome: "A-HA", nome: "Anna", classe: "1A", gruppo: "", room: "semi" }
+];
+
