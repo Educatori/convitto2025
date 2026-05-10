d3b4317a9cf3d411ddaa8821030c35f0a1e6db6b
Educatori <educatori127@gmail.com>
2026-05-10 18:39:31 +0200

    Update firebase-cruscotto-config.js

--- js/firebase-cruscotto-config.js
+++ js/firebase-cruscotto-config.js
@@ -1,18 +1,19 @@
 
 // Configurazione Firebase
- const firebaseConfig = {
-            apiKey: "AIzaSyAKBQJYiu1ConaCKOj_rlKQSOQaGVTinCc",
-            authDomain: "cruscotto-438bc.firebaseapp.com",
-            databaseURL: "https://cruscotto-438bc-default-rtdb.europe-west1.firebasedatabase.app",
-            projectId: "cruscotto-438bc",
-            storageBucket: "cruscotto-438bc.firebasestorage.app",
-            messagingSenderId: "142955041436",
-            appId: "1:142955041436:web:22be8db6e293bf14c55359",
-            measurementId: "G-LK93GN5JNP"
-        };
-// Inizializza Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const firebaseConfig = {
+  apiKey: "AIzaSyCujVjW7Aorn0illq_2w50u8oAgWGJEBRY",
+  authDomain: "cruscotto-722bc.firebaseapp.com",
+  databaseURL: "https://cruscotto-722bc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cruscotto-722bc",
+  storageBucket: "cruscotto-722bc.firebasestorage.app",
+  messagingSenderId: "594198664103",
+  appId: "1:594198664103:web:cc50982af1a9fc757b5c82",
+  measurementId: "G-BVS9JG6HVC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Variabili globali per la sincronizzazione
 let isSyncing = false;
